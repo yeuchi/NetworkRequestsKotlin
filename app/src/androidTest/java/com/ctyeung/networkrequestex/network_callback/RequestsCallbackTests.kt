01cc209dd9f5aed22d1ba18bb1940ec9c39ee820
Chi Yeung <yeun0007@umn.edu>
2020-06-25 20:20:44 -0500

    callback test

--- app/src/androidTest/java/com/ctyeung/networkrequestex/network_callback/RequestsCallbackTests.kt
+++ app/src/androidTest/java/com/ctyeung/networkrequestex/network_callback/RequestsCallbackTests.kt
@@ -6,16 +6,33 @@ import org.junit.Test
 import org.junit.runner.RunWith
 //import kotlinx.coroutines.test.runBlockingTest
 import junit.framework.Assert.assertEquals
+import java.util.*
 
 @RunWith(AndroidJUnit4ClassRunner::class)
 class RequestsCallbackTests {
+    val timer = Timer()
 
     @Test
     fun getUsers() {
         var request = RequestsCallback()
         request.getUsers()
 
-        Espresso.onIdle()
-      //  assertEquals(64, request.list?.size)
+        /*
+         * Wait 1 second for network request to complete
+         * ... Ideally, if there is UI, espresso will automatically wait for it to complete.
+         */
+        var i = 0
+        timer.scheduleAtFixedRate(
+            object : TimerTask() {
+
+                override fun run() {
+                    if(i==1)
+                      assertEquals(64, request.list?.size)
+
+                    i++
+                }
+            },
+            0, 1000
+        )
     }
 }
